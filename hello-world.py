abbea34bd6580cd75f464554c3623bb64b8a6544
Pauls Jakovels <pauls.jakovels@gmail.com>
2022-06-04 14:33:22 +0300

    forever

--- hello-world.py
+++ hello-world.py
@@ -3,9 +3,12 @@ import time
 from clearml import Task
 task = Task.init(project_name='examples', task_name='hello world3')
 
-#while True:
-for i in range(3):
-    print("mupse")
+logger = task.get_logger()
+
+while True:
+#for i in range(3):
+    #print("mupse")
+    logger.report_text("some message!")
     time.sleep(1)
 
 
